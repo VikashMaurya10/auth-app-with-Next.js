91bdb10c9c0757f2e9f4239966b829a56653f590
VikashMaurya10 <VikashMaurya10stp@gmail.com>
2023-10-02 09:31:59 +0530

    for git hub

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  distDir: "export",
+  // distDir: "export",
   // async redirects() {
   //   return [
   //     {
