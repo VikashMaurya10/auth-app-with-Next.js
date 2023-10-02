02192f38b64ec9f67881a08d29975751e78a425d
VikashMaurya10 <VikashMaurya10stp@gmail.com>
2023-10-02 09:50:36 +0530

    add env in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  env: {
+    URI: "mongodb+srv://vikashmauryastp:67NSKmY93jp0GxB5@cluster0.poglb9n.mongodb.net/",
+    SECRET_KEY:"nowsecretkeyHere"
+  },
   // distDir: "export",
   // async redirects() {
   //   return [
